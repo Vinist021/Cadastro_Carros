c1c191821d0d8eae5e9ee7816078d636c0ff40eb
Vinicius Teixeira <vinist.002@gmail.com>
2025-08-24 12:34:43 -0300

    Inicialização do projeto

---
+++ src/app/components/carro/carro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carro',
+  standalone: false,
+  templateUrl: './carro.html',
+  styleUrl: './carro.css'
+})
+export class Carro {
+
+}
