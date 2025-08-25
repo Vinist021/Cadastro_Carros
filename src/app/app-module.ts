a20faf3a96ed0bd2caa74d0ae49ab23ef8293a63
Vinicius Teixeira <vinist.002@gmail.com>
2025-08-25 14:29:57 -0300

    Criado formulário para cadastro de carros

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,6 +7,7 @@ import { CarroComponent } from './components/carro/carro';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header';
 import { CarrosComponent } from './components/carros/carros';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CarrosComponent } from './components/carros/carros';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
