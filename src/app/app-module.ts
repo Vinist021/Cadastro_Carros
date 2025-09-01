1a29b424c76ab5c5e2aa6fa40381ab0830bf7479
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-01 12:15:27 -0300

    Adicionada máscara de preço(R$)

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,13 +1,19 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing-module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+import { LOCALE_ID } from '@angular/core';
+import locale_PT from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
 import { App } from './app';
 import { CarroComponent } from './components/carro/carro';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header';
 import { CarrosComponent } from './components/carros/carros';
-import { FormsModule } from '@angular/forms';
+
+registerLocaleData(locale_PT, 'pt');
 
 @NgModule({
   declarations: [
@@ -24,7 +30,8 @@ import { FormsModule } from '@angular/forms';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection()
+    provideZonelessChangeDetection(),
+    {provide: LOCALE_ID, useValue: 'pt'}
   ],
   bootstrap: [App]
 })
